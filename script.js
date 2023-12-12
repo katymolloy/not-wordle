83463849371db1cd180accdd58401a7ad9f2aa7f
katyisonline <katyxmolloy@gmail.com>
2023-12-12 16:59:49 -0500

    Added word file
    The word to found is now working; added jQuery

--- script.js
+++ script.js
@@ -1,8 +1,29 @@
-const randomNum = () =>{
-    let index = (Math.random());
-    console.log(index)
-}
 
-// gen random num
-// get word at index
-// wordle shit 
+const ALL_WORDS = 14855;
+var wordToFind = '';
+
+(function randomNum() {
+    let index = Math.floor(Math.random() * (ALL_WORDS + 1));
+    console.log(index)
+    $.get('./data/valid-wordle-words.txt', function (data) {
+        let allWords = data.split("\n")
+        wordToFind = allWords[index]
+        console.log(wordToFind)
+    }, 'text');
+})();
+
+
+
+
+$('.letterInput').keyup(function () {
+    if ($(this).val().length == $(this).attr("maxlength")) {
+        $(this).next('.letterInput').focus();
+    }
+});
+
+
+$('#clearFields').click(function () {
+    $('.letterInput').val('')
+});
+
+
