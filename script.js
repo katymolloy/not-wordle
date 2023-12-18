c375c81b99db6cf64a525b8d06ca3f13f57ad9ea
katyisonline <katyxmolloy@gmail.com>
2023-12-18 18:09:30 -0500

    Color hints are now working
    Next challenge is adding a modal

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 
 const ALL_WORDS = 14855;
+var everyWord = []
 var wordToFind = '';
 var wordAttempt = '';
 var workingDivs = [];
 var divIdIndex = 0
 const divFocus = $('.letterInput')
-console.log(divFocus)
-
 
 function randomNum() {
     let index = Math.floor(Math.random() * (ALL_WORDS + 1));
 
     $.get('./data/valid-wordle-words.txt', function (data) {
-        let allWords = data.split("\n")
-        wordToFind = allWords[index]
+        everyWord = data.split("\n");
+
+        wordToFind = everyWord[index]
         // take out before adding to vercel - no cheating on my watch
         console.log(wordToFind)
     }, 'text');
@@ -27,8 +27,8 @@ $(document).keypress(function (e) {
     if (e.which !== 13 && e.which !== 8) {
         if (wordAttempt.length < 5) {
             $(divFocus[divIdIndex]).text(e.key.toUpperCase());
-            workingDivs.push(divFocus[divIdIndex].id)
-            // console.log(workingDivs)
+            workingDivs.push('#' + divFocus[divIdIndex].id)
+
             wordAttempt += e.key;
             divIdIndex++;
         } else {
@@ -44,15 +44,22 @@ $(document).keypress(function (e) {
 $(document).keydown(function (e) {
     switch (e.which) {
         case 13:
-            console.log(workingDivs)
-            checkAnswer(wordAttempt);
-            wordAttempt = '';
+            // ensure word input is valid
+            if ($.inArray(wordAttempt, everyWord) !== -1) {
+                checkAnswer(wordAttempt);
+                wordAttempt = '';
+
+            } else {
+                $(workingDivs[0]).parent().addClass('wrongAnswer')
+                workingDivs = [];
+            }
+            $(workingDivs[0]).parent().removeClass('wrongAnswer')
             break;
         case 8:
             if (wordAttempt.length > 0) {
                 wordAttempt = wordAttempt.slice(0, -1)
                 divIdIndex--
-
+                workingDivs.splice(-1, 1)
                 $(divFocus[divIdIndex]).empty();
                 break;
             }
@@ -63,23 +70,33 @@ $(document).keydown(function (e) {
 
 
 const checkAnswer = (word) => {
-    console.log(word, wordToFind)
-    workingDivs = [];
-    for (let i = 0; i < word.length; i++) {
-        if (word[i] === wordToFind[i]) {
-            console.log('here')
-        } else if (wordToFind.includes(word[i])) {
-            console.log('close match')
-        }
+    console.log(word, wordToFind, workingDivs)
+
+    switch (word === wordToFind) {
+        case false:
+            for (let i = 0; i < word.length; i++) {
+                if (word[i] === wordToFind[i]) {
+                    $(workingDivs[i]).addClass('correctGuess')
+                }else if(wordToFind.includes(word[i])){
+                    $(workingDivs[i]).addClass('closeGuess')
+                }else{
+                    $(workingDivs[i]).addClass('wrongGuess')
+                }
+            }
+            workingDivs = [];
+            break
+            ;
+        case true:
+
+            celebration();
+            break;
     }
+}
 
-    // for (let i = 0; i < word.length; i++) {
-    //     // console.log(word[i] + ' ' + wordToFind[i])
-    //     if (word[i] === wordToFind[i]) {
-    //         $(divFocus.id).filter(function() {
-    //             return $(this).text().includes(word[i]);
-    //           }).addClass('correctGuess');
-    //     }
-    // }
-
+const celebration = () => {
+    for (let i = 0; i < wordAttempt.length; i++) {
+        $(workingDivs[i]).addClass('correctGuess')
+    }
+   $('body').addClass('hellYeah')
+    workingDivs = [];
 }
